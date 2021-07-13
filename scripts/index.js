e13ec42b9e37f0e423adfc51d96411ecf54660ef
ungaf <ungaf@ya.ru>
2021-07-13 12:13:08 +0300

    fixed some bugs, added new page

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,17 @@
+arg = document.URL.split('#');
+
+if (arg[1] != undefined) {
+  $(".content").hide()
+  $(".item").removeClass("active")
+  $('#' + arg[1] + '_cnt').show()
+  $('#' + arg[1]).addClass("active")
+}
+
 $('.item').on('click', function (e) {
     e.preventDefault()
     $(".content").hide()
     $(".item").removeClass("active")
     $('#' + this.id + '_cnt').show()
-    this.addClass("active")
+    $('#' + this.id).addClass('active')
+    window.location.replace("#"+this.id)
   })
